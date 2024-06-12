707424d684bc9d1318c5ef9e667d67a67ac5624c
gourav-shinde <gauravsanjayshinde@gmail.com>
2024-06-12 10:12:02 -0400

    added build scripts

--- buildUnified.sh
+++ buildUnified.sh
@@ -4,8 +4,8 @@ cd /home/gaurav/workspace/warped2
 make clean
 autoreconf -i
 # ./configure --with-mpi-includedir=/usr/include/x86_64-linux-gnu/mpich --with-mpi-libdir=/usr/lib/x86_64-linux-gnu/mpich --prefix=/home/gaurav/workspace/warped2/build --with-unified-queue --enable-debug
-# ./configure --with-mpi-includedir=/usr/include/x86_64-linux-gnu/mpich --with-mpi-libdir=/usr/lib/x86_64-linux-gnu/mpich --prefix=/home/gaurav/workspace/warped2/build --with-unified-queue 
-./configure --with-mpi-includedir=/usr/include/x86_64-linux-gnu/mpich --with-mpi-libdir=/usr/lib/x86_64-linux-gnu/mpich --prefix=/home/gaurav/workspace/warped2/build
+./configure --with-mpi-includedir=/usr/include/x86_64-linux-gnu/mpich --with-mpi-libdir=/usr/lib/x86_64-linux-gnu/mpich --prefix=/home/gaurav/workspace/warped2/build --with-unified-queue 
+# ./configure --with-mpi-includedir=/usr/include/x86_64-linux-gnu/mpich --with-mpi-libdir=/usr/lib/x86_64-linux-gnu/mpich --prefix=/home/gaurav/workspace/warped2/build
 make
 make install
 
