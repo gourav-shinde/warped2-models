67b0ef9cd72bea1a41dabe6ad2c92911657dde11
gourav-shinde <gauravsanjayshinde@gmail.com>
2024-06-17 12:40:36 -0400

    buildHoni file added

---
+++ buildHoni.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd /home/gaurav/workspace/warped2
+make clean
+autoreconf -i
+# ./configure --with-mpi-includedir=/usr/lib/x86_64-linux-gnu/openmpi/include --prefix=/home/gaurav/workspace/warped2/build --with-unified-queue --enable-debug
+# ./configure --with-mpi-includedir=/usr/lib/x86_64-linux-gnu/openmpi/include --prefix=/home/gaurav/workspace/warped2/build --with-unified-queue 
+./configure --with-mpi-includedir=/usr/lib/x86_64-linux-gnu/openmpi/include --prefix=/home/gaurav/workspace/warped2/build
+make
+make install
+
+cd /home/gaurav/workspace/warped2-models/
+make clean
+autoreconf -i 
+./configure --with-warped=/home/gaurav/workspace/warped2/build CXXFLAGS='-g -O3 -std=c++11' CXX=mpicxx
+make
+
