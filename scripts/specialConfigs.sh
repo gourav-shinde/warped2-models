6b6827c2e912eb8190c654eff04e57aaa4475b1c
gourav-shinde <gauravsanjayshinde@gmail.com>
2024-06-22 11:09:23 -0400

    hello

--- scripts/specialConfigs.sh
+++ scripts/specialConfigs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Allows batch runs of simulations. Saves results to log files
-
-for config in config/gaurav/* ; do
+folder=$1
+for config in config/$1/* ; do
     echo $config
     ./configSimulate.sh $config
 done
